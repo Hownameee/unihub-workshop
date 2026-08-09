5a699eb4e7d8defaed68ff7ad733f04fb517ec7d
Hownameee <namhaohuynh@gmail.com>
2026-08-09 23:51:39 +0700

    feat(frontend): configure custom theme, inter font, favicon, and router

--- app-frontend/src/main.tsx
+++ app-frontend/src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router'
+import router from './router/router'
 
 const root = document.getElementById('root')
 if (!root) {
@@ -10,6 +11,6 @@ if (!root) {
 
 createRoot(root).render(
 	<StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</StrictMode>,
 )
---
+++ app-frontend/src/router/router.tsx
@@ -0,0 +1,7 @@
+import { createBrowserRouter } from 'react-router'
+import Home from '../pages/home/Home'
+import Layout from '../pages/Layout'
+
+const router = createBrowserRouter([{ path: '/', element: <Layout />, children: [{ index: true, element: <Home /> }] }])
+
+export default router
